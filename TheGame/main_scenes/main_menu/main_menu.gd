4795026b5b32b8b68797b9fbdc6cc09fbe20a729
Kostiantyn <knifantiev@gmail.com>
2021-01-30 14:22:57 +0200

    Level selection has been added

--- TheGame/main_scenes/main_menu/main_menu.gd
+++ TheGame/main_scenes/main_menu/main_menu.gd
@@ -4,12 +4,26 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+export(Array, String) var level_names
+export(Array, String) var level_path
 
+onready var levels_ui = $CanvasLayer/MarginContainer/HBoxContainer/Levels
+onready var menu_ui = $CanvasLayer/MarginContainer/HBoxContainer/MenuButtons
 
+var level_selector_prefab = preload("res://prefab_scenes/LevelSelectorButton/LevelSelectorButton.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+    pass
 
+    if len(level_names) != len(level_path):
+        print("Level names and level pathes should be the same length!")
+    else:
+        for i in range(len(level_names)):
+            var new_button = level_selector_prefab.instance()
+            levels_ui.add_child(new_button)
+            new_button.text = level_names[i]
+            new_button.stored_level_name = level_path[i]
+            new_button.connect("selected", self, "_on_level_selected")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -28,3 +42,17 @@ func _on_tutorial_pressed():
 
 func _on_exit_pressed():
     get_tree().quit()
+
+
+func _on_level_selected(level):
+    GSceneManager.goto_scene_wloader(level)
+
+
+func _on_level_selector_pressed():
+    menu_ui.hide()
+    levels_ui.show()
+
+
+func _on_Return_pressed():
+    menu_ui.show()
+    levels_ui.hide()
