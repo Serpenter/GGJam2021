4a6d9ce8c22eb721696f37b14e998f153fbfd99d
gorko <gorko438@gmail.com>
2021-01-30 03:49:23 +0200

    forcefields update

--- TheGame/prefab_scenes/ForcefieldPanel/ForcefieldPanel.gd
+++ TheGame/prefab_scenes/ForcefieldPanel/ForcefieldPanel.gd
@@ -2,18 +2,38 @@ extends Node2D
 
 
 onready var interaction_area = $InteractionArea
-onready var area_sprite = $AreaSprite
-onready var button_sprite = $ButtonSprite
+onready var sprite = $Sprite
+onready var animation_player = $AnimationPlayer
+onready var light = $Lights/Light2D
 
 export var forcefield_name = "green"
 
 export var activation_groups = ["Ball"]
 
+export(bool) var is_alignable = true
+# presistent parameters - to be saved-restored-loaded
+var initial_state
+
+func get_item_ui_data():
+    return $ItemUIData
+
+func save_initial_state():
+    initial_state = {
+        "initial_rotation": rotation
+    }
+
+func load_state(state):
+    rotation = initial_state["initial_rotation"]
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    area_sprite.modulate = Forcefields.get_forcefield_color(forcefield_name)
-    button_sprite.modulate = Forcefields.get_forcefield_color(forcefield_name)
+#    area_sprite.modulate = Forcefields.get_forcefield_color(forcefield_name)
+    var color  = Forcefields.get_forcefield_color(forcefield_name)
+    sprite.modulate = color
+    light.color = color
+    animation_player.play("idle")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- TheGame/prefab_scenes/ForcefieldWall/ForcefieldWall.gd
+++ TheGame/prefab_scenes/ForcefieldWall/ForcefieldWall.gd
@@ -1,17 +1,36 @@
 extends StaticBody2D
 
 onready var sprite = $Sprite
-
+onready var light = $Light2D
 export var forcefield_name = "green"
+
+export(bool) var is_alignable = true
+# presistent parameters - to be saved-restored-loaded
+var initial_state
+
+func get_item_ui_data():
+    return $ItemUIData
+
+func save_initial_state():
+    initial_state = {
+        "initial_rotation": rotation
+    }
+
+func load_state(state):
+    rotation = initial_state["initial_rotation"]
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-    sprite.modulate = Forcefields.get_forcefield_color(forcefield_name)
+func _ready():   
+    var color = Forcefields.get_forcefield_color(forcefield_name)
     var layer = Forcefields.get_forcefield_layer(forcefield_name)
     set_collision_layer_bit(layer, true)
-
+    sprite.modulate = color
+    light.color = color
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+
